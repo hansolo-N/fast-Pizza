4eae50732c36b949d1d377b0100ba7e9df7dfa29
hansolo-N <91372832+hansolo-N@users.noreply.github.com>
2023-10-31 12:14:57 +0200

    created action for createOrder route when submitting data for order

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Form } from "react-router-dom";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -37,8 +38,8 @@ function CreateOrder() {
   return (
     <div>
       <h2>Ready to order? Let's go!</h2>
-
-      <form>
+      {/* Form method="POST" action="" */}
+      <Form method="POST" action="">
         <div>
           <label>First Name</label>
           <input type="text" name="customer" required />
@@ -72,9 +73,16 @@ function CreateOrder() {
         <div>
           <button>Order now</button>
         </div>
-      </form>
+      </Form>
     </div>
   );
+}
+
+export async function action({request}){
+const formData = await request.formData()
+const data = Object.fromEntries(formData)
+console.log(data)
+return null
 }
 
 export default CreateOrder;
