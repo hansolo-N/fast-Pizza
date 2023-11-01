61a08f965960eeeaa981e7a48c34d6909afe14f7
hansolo-N <91372832+hansolo-N@users.noreply.github.com>
2023-11-01 13:21:32 +0200

    added responsive design to createUser and cartoverview component

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 
 function CartOverview() {
   return (
-    <div className='bg-red-700'>
-      <p className=" text-neutral-50 font-semibold uppercase">
+    <div className='bg-red-700 text-stone-50 p-4 uppercase md:text-base sm:px-6'>
+      <p className=" text-neutral-50 font-semibold uppercase space-x-4 sm:space-x-6 ">
         <span >23 pizzas</span>
         <span >$23.45</span>
       </p>
