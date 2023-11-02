f481fd99c8cad7b5ee8c45e39914c2e308a2b206
hansolo-N <91372832+hansolo-N@users.noreply.github.com>
2023-11-02 15:25:10 +0200

    styling of cart: added secondary styling for buttons and styling list items in the cart

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,6 +1,7 @@
-import { Link } from 'react-router-dom';
+
 import LinkButton from '../ui/LinkButton';
 import Button from '../ui/Button';
+import CartItem from './CartItem'
 const fakeCart = [
   {
     pizzaId: 12,
@@ -29,15 +30,17 @@ function Cart() {
   const cart = fakeCart;
 
   return (
-    <div>
+    <div className='px-4 py-3'>
       <LinkButton to="/menu" className='text-sm text-blue-500 hover:text-blue-700 hover:underline'>&larr; Back to menu</LinkButton>
 
-      <h2>Your cart, %NAME%</h2>
-
-      <div>
+      <h2 className='mt-7 text-xl font-semibold'>Your cart, %NAME%</h2>
+      <ul className='mt-3 divide-y divide-stone-200 border-b'>
+        {fakeCart.map(item=><CartItem item={item} key={item.pizzaId}/>)}
+      </ul>
+      
+      <div className='mt-7 space-x-2'>
         <Button to="/order/new" type='primary'>Order pizzas</Button>
-        
-        <button>Clear cart</button>
+        <Button type='secondary'>Clear Cart</Button>
       </div>
     </div>
   );
