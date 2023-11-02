1dad2becab78a9577967b0c005cdf7c3516b0573
hansolo-N <91372832+hansolo-N@users.noreply.github.com>
2023-11-02 11:38:07 +0200

    styled loader component

--- src/features/ui/Loader.jsx
+++ src/features/ui/Loader.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Loader() {
   return (
-    <div className='loader'>Loader</div>
+    <div className='absolute bg-slate-200/20 inset-0 backdrop-blur-sm flex items-center justify-center'>
+        <div className='loader'></div>
+    </div>
+
   )
 }
 
