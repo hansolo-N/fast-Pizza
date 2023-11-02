2edc4372e2ee4c6c22b97beac104ae7ae019afd9
hansolo-N <91372832+hansolo-N@users.noreply.github.com>
2023-11-02 11:23:11 +0200

    created linkButton component and refactored button to work as a link

--- src/features/ui/Button.jsx
+++ src/features/ui/Button.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 
-function Button({children,disabled}) {
+function Button({children,disabled,to}) {
+
+  const className = "bg-red-600 uppercase font-semibold text-stone-800 py-3 px-4 inline-block tracking-wide rounded-full hover:bg-red-400 transition-colors duration-300 focus:outline-none focus:ring focus:ring-red-600 focus:bg-red-600 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4"
+
+
+
+  if(to) return <Link to={to} className={className}>{children}</Link>
+  
   return (
-    <button disabled={disabled} className="bg-red-600 
-    uppercase font-semibold
-     text-stone-800 py-3 px-4 inline-block tracking-wide 
-     rounded-full hover:bg-red-400 
-     transition-colors duration-300 focus:outline-none focus:ring focus:ring-red-600 focus:bg-red-600 focus:ring-offset-2
-     disabled:cursor-not-allowed">
-        {children}
+    <button disabled={disabled}className={className}>{children}
     </button>
   )
 }
