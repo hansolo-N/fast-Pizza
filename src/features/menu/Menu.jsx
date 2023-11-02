352ce876bb352ed604e2fe67e366e401f5495400
hansolo-N <91372832+hansolo-N@users.noreply.github.com>
2023-11-02 13:09:38 +0200

    configured tailwind default font to roboto

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -7,7 +7,9 @@ function Menu() {
   const menu = useLoaderData()
 
 
-  return <ul>{menu.map(pizza=><MenuItem pizza={pizza} key={pizza.id}/>)}</ul>;
+  return <ul className="divide-y divide-stone-200 px-2">
+            {menu.map(pizza=><MenuItem pizza={pizza} key={pizza.id}/>)}
+      </ul>;
 }
 
 export async function loader(){
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,14 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
+  theme: {fontFamily:{
+    sans: 'Roboto Mono, monospace'
+  },
+    extend: {
+      height:{
+        screen:'100dvh'
+      }
+    },
   },
   plugins: [],
 }
