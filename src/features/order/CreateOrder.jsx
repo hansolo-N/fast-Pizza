b040069ea9d9025b620e1a056c9b9f5a935709ae
hansolo-N <91372832+hansolo-N@users.noreply.github.com>
2023-11-03 11:31:47 +0200

    styling create order component: adjusted input boxes, font and spacing of elements and lastly changed favicon

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -44,32 +44,32 @@ function CreateOrder() {
   const cart = fakeCart;
 
   return (
-    <div>
-      <h2>Ready to order? Let's go!</h2>
+    <div className="py-6 px-4">
+      <h2 className="text-xl font-semibold mb-8">Ready to order? Let's go!</h2>
       {/* Form method="POST" action="" */}
       <Form method="POST" action="">
-        <div>
-          <label>First Name</label>
-          <input className="input" type="text" name="customer" required />
+
+        <div className="mb-5 flex gap-2 flex-col sm:flex-row sm:items-center">
+          <label className="sm:basis-40 ">First Name</label>
+          <input className="input grow" type="text" name="customer" required />
         </div>
 
-        <div>
-          <label>Phone number</label>
-
-          <div>
-            <input className="input" type="tel" name="phone" required />
-          </div>
-          {formErrors?.phone && <p>{formErrors.phone}</p>}
-        </div>
-
-        <div>
-          <label>Address</label>
-          <div>
-            <input  type="text" name="address" required  className="input"/>
+        <div className="mb-5 flex gap-2 flex-col sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Phone number</label>
+          <div className="grow">
+            <input className="input w-full" type="tel" name="phone" required />
+          {formErrors?.phone && <p className="text-xs mt-2 text-red-700 bg-red-100 p-2 rounded-md">{formErrors.phone}</p>}
           </div>
         </div>
 
-        <div>
+        <div className="mb-5 flex gap-2 flex-col sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Address</label>
+          <div className="grow">
+            <input class type="text" name="address" required  className="input w-full"/>
+          </div>
+        </div>
+
+        <div className="mb-12 flex gap-5 items-center">
           <input
             type="checkbox"
             name="priority"
@@ -78,7 +78,7 @@ function CreateOrder() {
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
           />
-          <label htmlFor="priority">Want to yo give your order priority?</label>
+          <label className="font-medium" htmlFor="priority">Want to yo give your order priority?</label>
         </div>
 
         <div>
--- src/features/ui/Button.jsx
+++ src/features/ui/Button.jsx
@@ -4,13 +4,13 @@ import { Link } from 'react-router-dom';
 function Button({children,disabled,to,type}) {
 
 
-  const base = "bg-red-600 uppercase font-semibold text-stone-800 inline-block tracking-wide rounded-full hover:bg-red-400 transition-colors duration-300 focus:outline-none focus:ring focus:ring-red-600 focus:bg-red-600 focus:ring-offset-2 disabled:cursor-not-allowed"
+  const base = "bg-red-600 text-sm uppercase font-semibold text-stone-800 inline-block tracking-wide rounded-full hover:bg-red-400 transition-colors duration-300 focus:outline-none focus:ring focus:ring-red-600 focus:bg-red-600 focus:ring-offset-2 disabled:cursor-not-allowed"
 
   
   const styles = {
     
     primary : base + ' px-4 py-3 md:px-6 md:py-4',
-    secondary: "border-2 border-stone-300 uppercase font-semibold text-stone-800 inline-block tracking-wide rounded-full hover:text-stone-800 hover:bg-stone-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-stone-200 focus:bg-stone-600 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5",
+    secondary: "border-2 border-stone-300 text-sm uppercase font-semibold text-stone-800 inline-block tracking-wide rounded-full hover:text-stone-800 hover:bg-stone-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-stone-200 focus:bg-stone-600 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5",
     small: base + ' px-4 py-2 md:px-5 md:py-2.5'
   }
 
