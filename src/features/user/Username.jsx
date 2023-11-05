672cb4d212647c4db66c9b101da6ca7fd7993f2f
hansolo-N <91372832+hansolo-N@users.noreply.github.com>
2023-11-05 08:56:39 +0200

    on create user compnent added dispatch function to update name in redux store and navigate user to menu afterwards

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -3,6 +3,9 @@ import { useSelector} from 'react-redux/es/hooks/useSelector'
 
 function Nasr() {
   const username = useSelector(state=>state.user.username)
+
+  if(!username) return null
+
   return (
     <div className='text-sm font-semibold md:block'>{username}</div>
   )
