002f6db8433428d60fbfc714e64d5b7c6ae406ff
hansolo-N <91372832+hansolo-N@users.noreply.github.com>
2023-11-06 14:25:46 +0200

    menu item: added functionality to remove item from menu only if it exists in the cart

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -44,4 +44,8 @@ export const getTotalQuantity=(state)=>state.cart.cart.reduce((sum,item)=>sum + 
 
 export const getTotalCartPrice=(state)=>state.cart.cart.reduce((sum,item)=>sum + item.totalPrice,0)
 
+
+
+export const getCurrentQuantityById =(id)=>(state)=>state.cart.cart.find(item=>item.pizzaId === id )?.quantity ?? 0;
+
 export const getCart=(state)=>state.cart.cart
--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,11 +1,16 @@
 import { formatCurrency } from "../utilities/helpers";
 import Button from '../ui/Button'
-import { addItem } from "../cart/cartSlice";
+import { addItem, getCurrentQuantityById } from "../cart/cartSlice";
 import { useDispatch } from "react-redux";
+import DeleteItem from '../cart/DeleteItem'
+import { useSelector } from "react-redux";
 
 function MenuItem({ pizza }) {
   const dispatch = useDispatch()
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  const currentQuantity = useSelector(getCurrentQuantityById(id))
+
+  const isInCart = currentQuantity > 0
 
   function handleAddToCart(){
     const newItem = {
@@ -26,7 +31,10 @@ function MenuItem({ pizza }) {
         <div className="mt-auto flex items-center justify-between">
           {!soldOut ? <p className="text-sm">{formatCurrency(unitPrice)}</p> : <p className="text-sm uppercase font-medium">Sold out</p>}
         
-        {!soldOut && <Button type='small'handleClick={handleAddToCart}>Add to Cart</Button>}
+        { isInCart &&
+          <DeleteItem pizzaId={id}/>}
+
+        {!soldOut && !isInCart && <Button type='small'handleClick={handleAddToCart}>Add to Cart</Button>} 
         </div>
       </div>
     </li>
