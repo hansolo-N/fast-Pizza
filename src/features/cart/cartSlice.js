52985d1357ff23a891b7d5225a2b177f98921b8c
hansolo-N <91372832+hansolo-N@users.noreply.github.com>
2023-11-06 10:35:08 +0200

    updated cartSlice: added functions to add ,delete increase/decrease item quantity and clear cart functionality, import cart slice to reducer

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -2,17 +2,46 @@ import { createSlice } from "@reduxjs/toolkit"
 
 
 const initialState = {
-cart:[]
+cart:[
+    {
+        pizzaId: 12,
+        name: 'Mediterranean',
+        quantity:2,
+        unitPrice:16,
+        totalPrice:32,
+    }
+]
 }
 
 const cartSlice = createSlice({
     name:'cart',
     initialState: initialState,
     reducers:{
+        addItem(state,action){
+            //payload = newItem
+            state.cart.push(action.payload)
+        },
+        removeItem(state,action){
+            //payload = itemId
+            state.cart = state.cart.filter((item)=>item.pizzaId !== action.payload)
+        },
+        increaseItemQuantity(state,action){
+            const item = state.cart.find((item)=> item.id === action.payload)
+            item.quantity++;
+            item.totalPrice = item.unitPrice * item.quantity
+        },
+        decreaseItemQuantity(state,action){
+            const item = state.cart.find((item)=> item.id === action.payload)
+            item.quantity--;
+            item.totalPrice = item.unitPrice * item.quantity
+        },
+        clearCart(state){
+            state.cart = []
+        },
 
     }
 })
 
 export default cartSlice.reducer
 
-export {} from cartSlice.actions
+export {addItem,removeItem,increaseItemQuantity,decreaseItemQuantity,clearCart} from cartSlice.actions
